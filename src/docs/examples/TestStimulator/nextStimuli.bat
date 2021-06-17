70fd3afc52c7dbb8ae8cb0909c31bc8bd9c8c503
Hartmut Schorrig <hartmut@vishia.org>
2021-06-17 22:30:07 +0200

    Progress StimuliSelector

--- src/docs/examples/TestStimulator/nextStimuli.bat
+++ src/docs/examples/TestStimulator/nextStimuli.bat
@@ -1,7 +1,27 @@
+echo off
 REM any key sends "next"
+libs\socketcmd.exe
 :loop
+echo first wait for cmd from Stimuli Selection
+libs\socketcmd.exe -own:127.0.0.1:0xaff1 -dst:127.0.0.1:0xaff0 -rx test abort finish
+if errorlevel 1 goto :test
+if errorlevel 2 goto :abort
+if errorlevel 3 goto :finish
 
-libs\socmd.exe -own:127.0.0.1:0xaff1 -dst:127.0.0.1:0xaff0 step
+:test
+echo
+echo The test needs a moment, here for demonstration press any key if test is finished.
 echo abort: ctrl-C
-pause
+::pause
+REM the ping needs about 2 second, instead the test.
+ping 127.0.0.1
+REM if test is finished, send step to StimuliSelctor.
+libs\socketcmd.exe -own:127.0.0.1:0xaff1 -dst:127.0.0.1:0xaff0 -cmd step
 goto :loop
+:abort
+echo test was aborted
+exit /b
+:finish
+echo test finished
+exit /b
+
--- src/docs/examples/TestStimulator/stimuli.jzT.cmd
+++ src/docs/examples/TestStimulator/stimuli.jzT.cmd
@@ -61,7 +61,7 @@ sub genTestfiles(Map values, Map texts) {
 
 ##expected command from SocketCmd_vishia to execute the next step for gen test cases.
 ##
-String cmpNext = <:>"step"<.>;
+String cmpNext = <:>step<.>;
 
 
 
@@ -74,19 +74,26 @@ String cmpNext = <:>"step"<.>;
 ##  then the UDP socket connection is closed to abort the generation thread.
 ##
 sub btnGenTestcases(String select) {
- 
-  <+out>soRx=<&jztc.envar.soRx><.+n>
   if(jztc.envar.soRx) {    ##hint: use definitely the script variable, not the local copy.
     <+out>...abort genTestCases: <.+n> 
+    jztc.envar.soRx.tx("abort");
     jztc.envar.soRx.close();
     jztc.envar.soRx = null;
   } 
   else {
     <+out>generate test cases: .... <.+n> 
-    Obj testcases = java org.vishia.testutil.TestConditionCombi.prepareTestCases(select, 5);
-    call genTestCaseThread(testcases = testcases);
+    call genTestCaseThread(select=select);
   }
 }
+
+
+
+##
+##colors for button genTestCases
+Obj colorGenTestcaseActive = java org.vishia.gral.ifc.GralColor.getColor("lgn");
+Obj colorGenTestcaseError = java org.vishia.gral.ifc.GralColor.getColor("lrd");
+Obj colorGenTestcaseWaitRx = java org.vishia.gral.ifc.GralColor.getColor("lye");
+Obj colorGenTestcaseInactive = java org.vishia.gral.ifc.GralColor.getColor("wh");
 
 
 
@@ -95,25 +102,52 @@ sub btnGenTestcases(String select) {
 ##Hint: The sub routine is the wrapper arround the thread.
 ##      The sub routine itself is finished immediately, necessary because it is calling in the GUI thread.
 ##
-sub genTestCaseThread(Obj testcases) {
+sub genTestCaseThread(String select) {
   Thread execThread = {                        ## This thread generates one test case in each for loop
-    jztc.envar.soRx = java new org.vishia.communication.GetRx_InterProcessComm("UDP:127.0.0.1:45040");
-    <+out>Thread .... <&jztc.envar.soRx><.+n> 
+    String sIpOwn="UDP:127.0.0.1:45040";
+    String sIpDst="UDP:127.0.0.1:45041";
+    jztc.envar.soRx = java new org.vishia.communication.GetRx_InterProcessComm(sIpOwn, sIpDst);
     Bool contFor = true;                       ## possibility to abort the generation
+    Obj testcases = java org.vishia.testutil.TestConditionCombi.prepareTestCases(select, 5);
     for(testcase: testcases && contFor ) {
+      jztc.envar.stimuliSelector.btnGenTestcases.setBackColor(colorGenTestcaseActive, 0);
+      jztc.envar.stimuliSelector.btnGenTestcases.setText("abort generate");
       String name = <:><:for:var:testcase><&var.sel><:hasNext>_<.hasNext><.for><.>; 
       <+out>test case: <&name><.+n> 
       Obj lineValues = values.get(testcase[0].sel);
       Obj lineTexts = texts.get(testcase[1].sel);          ## generates the files for this case:
       call genTestfiles(values = lineValues, texts = lineTexts);
       ##
-      String next = jztc.envar.soRx.waitRx();  ## waits for a cmd received via socket:
-      <+out>msg from socket::<&next>::<.+n>
-      contFor = bool(next >= cmpNext);         ## repeats, generate next if "step" is received
+      Obj cmd1 = execThread.awaitcmd(1000);
+      jztc.envar.soRx.tx("test");
+      ##
+      Bool rxHasError = jztc.envar.soRx.hasError();
+      if(rxHasError) {
+        <+out>ERROR socket receive on <&sIpOwn>: <&jztc.envar.soRx.getState()><.+n>
+        contFor = false;
+      } else {
+        jztc.envar.stimuliSelector.btnGenTestcases.setBackColor(colorGenTestcaseWaitRx, 0);
+        jztc.envar.stimuliSelector.btnGenTestcases.setText("abort wait rx");
+        <+out>Thread waits for <&sIpOwn>: <&jztc.envar.soRx><.+n> 
+        String next = jztc.envar.soRx.waitRx();  ## waits for a cmd received via socket:
+        <+out>msg from socket::<&next>::<.+n>
+        contFor = bool(next >= cmpNext);         ## repeats, generate next if "step" is received
+      }
     }  
+    ##stateRx = jztc.envar.soRx.getNumState();
+    rxHasError = jztc.envar.soRx.hasError();
+    <+out>Thread finished<.+n>
+    if(rxHasError) {
+      jztc.envar.soRx.tx("error");
+      jztc.envar.stimuliSelector.btnGenTestcases.setBackColor(colorGenTestcaseError, 0);
+    } else {
+      jztc.envar.soRx.tx("finish");
+      jztc.envar.stimuliSelector.btnGenTestcases.setBackColor(colorGenTestcaseInactive, 0);
+    }
+    jztc.envar.stimuliSelector.btnGenTestcases.setText("gen test cases");
     jztc.envar.soRx.close();
     jztc.envar.soRx = null;
-    <+out>Thread finished<.+n>
+      
   }
   ##do not use execThread.join(0); because the wrapper routine should be immediately finished, called in the GUI thread!
 }
