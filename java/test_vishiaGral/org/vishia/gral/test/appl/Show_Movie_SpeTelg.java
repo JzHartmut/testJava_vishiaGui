96064b075db4887ef0f0efdebcde23cf022fb824
Hartmut Schorrig <hartmut.schorrig@vishia.org>
2022-11-01 22:27:37 +0100

    redraw dynamic with clean with image runs

--- java/test_vishiaGral/org/vishia/gral/test/appl/Show_Movie_SpeTelg.java
+++ java/test_vishiaGral/org/vishia/gral/test/appl/Show_Movie_SpeTelg.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.vishia.gral.base.GralButton;
 import org.vishia.gral.base.GralCanvasStorage;
 import org.vishia.gral.base.GralMng;
 import org.vishia.gral.base.GralPos;
@@ -24,10 +25,16 @@ public class Show_Movie_SpeTelg {
   
   final GralPos pos = new GralPos(this.gralMng, "@screen,10..80,10..120");
   
-  final GralWindow wind = new GralWindow(this.pos, "SpeTelgWindow", "SPE data transfer"
+  final GralWindow wind = new GralWindow(this.pos, "MainWindow", "SPE data transfer"
                         , GralWindow_ifc.windResizeable , this.gralMng);
   
-  final GralPlotArea plotArea = new GralPlotArea(this.pos, "@SpeTelg,0..-10,0..0=canvas");
+  final GralPlotArea plotArea = new GralPlotArea(this.pos, "@Main,0..-10,0..0=canvas");
+  
+  final GralButton btnRunStop = new GralButton(this.pos, "@-8+3,-15..-2=runStop");
+  { this.btnRunStop.setSwitchMode("run / stop?", "stop / run?");
+  }
+  
+  final GralButton btnStep = new GralButton(this.pos, "@-4+3,-15..-2=step", "step", null);
   
   final GralCanvasStorage plot = this.plotArea.getCanvasStore(0);
   
@@ -44,7 +51,7 @@ public class Show_Movie_SpeTelg {
    * +--+--+ </pre>
    * It is used for all Mem presentation (Master and Slave).
    */
-  final GralCanvasStorage.FigureDataSet figData_Words = new GralCanvasStorage.FigureDataSet("words"); { 
+  final GralCanvasStorage.FigureDataSet figData_Words = new GralCanvasStorage.FigureDataSet(); { 
     GralColor color = GralColor.getColor("bk");
     this.figData_Words.addPolyline(color).point(0, 0).point(0,2);  // |
     for(int ix = 0; ix <20; ++ix) {
@@ -63,7 +70,7 @@ public class Show_Movie_SpeTelg {
    * ^
    * | </pre>
    */
-  final GralCanvasStorage.FigureDataSet figData_RefTop = new GralCanvasStorage.FigureDataSet("refArrow"); { 
+  final GralCanvasStorage.FigureDataSet figData_RefTop = new GralCanvasStorage.FigureDataSet(); { 
     GralColor color = GralColor.getColor("drd");
     this.figData_RefTop.addPolyline(color).point(0, 0).point(0, 1.9f);  //                |
     this.figData_RefTop.addPolyline(color).point(-0.4f, 1).point(0, 2).point(0.4f, 1);  // ^
@@ -71,7 +78,7 @@ public class Show_Movie_SpeTelg {
   
   
   
-  final GralCanvasStorage.FigureDataSet figData_serialRight = new GralCanvasStorage.FigureDataSet("serialRight"); { 
+  final GralCanvasStorage.FigureDataSet figData_serialRight = new GralCanvasStorage.FigureDataSet(); { 
     GralColor color = GralColor.getColor("bk");
     this.figData_serialRight.addPolyline(color).point( 0, 0.3f).point(24, 0.3f);    // ==========
     this.figData_serialRight.addPolyline(color).point( 0, -0.3f).point(24, -0.3f);  // 
@@ -85,6 +92,11 @@ public class Show_Movie_SpeTelg {
   
   
   float posSerialRef;
+  
+  /**For the current communication. Related to {@link #colorWordsMaster}.
+   * If outside range, use white.
+   */
+  int ixColor;
   
   boolean bCont = true;
 
@@ -112,6 +124,14 @@ public class Show_Movie_SpeTelg {
   
   
   
+  GralColor colorIx(int ixColor, int station) {
+    GralColor[] colorArray = this.colorWordsMaster;
+    if(ixColor <0 || ixColor >= colorArray.length) {
+      return this.colorWhite;
+    } else return colorArray[ixColor];
+  }
+  
+  
   void init() throws ParseException {
     //
     this.pos.setPosition("10-2,10+1++");
@@ -121,7 +141,7 @@ public class Show_Movie_SpeTelg {
     this.plot.addFigure("dataWordsSlave1", this.pos, this.figData_Words, false);
 
     for(int ix = 0; ix < 20; ++ix) {                       // color (content) of the data words slave
-      this.rxSlave1[ix] = this.plot.addFigure("rxSlave1-" + ix, this.pos, new GralCanvasStorage.Fillin(colorWhite), false);
+      this.rxSlave1[ix] = this.plot.addFigure("rxSlave1-" + ix, this.pos, new GralCanvasStorage.Fillin("X", this.colorWhite), true);
       this.pos.setPosition(",+1");
     }
     
@@ -132,9 +152,11 @@ public class Show_Movie_SpeTelg {
       GralColor colorFill = GralColor.getColor(red, (19-ix)*10 + 65, ix*10 + 65);
       this.colorWordsMaster[ix] = colorFill;
     }
+    //---------------------------------------------------- //Fillin of the master tx words 
     this.pos.setPosition("10-2, 10+1++");
-    for(int ix = 0; ix <20; ++ix) {             //4 points per word  --+
-      this.plot.drawFillin(this.pos, this.colorWordsMaster[ix]);
+    for(int ix = 0; ix <20; ++ix) {                       
+      this.plot.addFigure("wordTxMaster", this.pos, new GralCanvasStorage.Fillin("X", this.colorWordsMaster[ix]), false);
+      //this.plot.drawFillin(this.pos, this.colorWordsMaster[ix]);
       this.pos.setNextPosition();
     }
     //GralPlotArea.UserUnits units = this.plot.userUnitsPerGrid(0.0f,  0.0f,  1.0f,  1.0f);
@@ -148,12 +170,14 @@ public class Show_Movie_SpeTelg {
     //
     this.pos.setPosition("13-2, 35+1");                    // value on the serial plug
     GralColor colorFillin = this.colorWordsMaster[8];
-    this.serialOut1word = this.plot.addFigure("wordTxMaster", this.pos, new GralCanvasStorage.Fillin(colorFillin), false);
+    this.serialOut1word = this.plot.addFigure("wordTxMaster", this.pos, new GralCanvasStorage.Fillin("X", colorFillin), true);
     //
     LogMessage log = new LogMessageStream(System.out);
     this.gralMng.createGraphic("SWT", 'B', log);
     
-    this.plotArea.repaint(100, 100);
+    this.posSerialRef = 24.0f;
+    
+    this.plotArea.redraw(100, 100);
   }
   
   
@@ -162,36 +186,39 @@ public class Show_Movie_SpeTelg {
    * @throws ParseException
    */
   void step() throws ParseException {
-    this.ref1.move(5, 0);                                  // move 0.5 grid to right
-    if((this.posSerialRef +=0.5) >=24) { 
-      if(this.posSerialRef >=25) {                         // waits a while without paint 
-        this.posSerialRef = 0;                             // then starts from 0 again
-        this.ref1.pos.setPosition("12-2, 10.0+1");         // start position, first increment is done before usage
-        for(GralCanvasStorage.Figure rx1 : this.rxSlave1 ) {
-          rx1.data.color = this.colorWhite;
+    //this.ref1.move(5, 0);                                  // move 0.5 grid to right
+    if(  this.btnRunStop.getState() == GralButton.State.On
+      || this.btnStep.wasPressed()) {
+      if((this.posSerialRef +=1.0) >=23) { 
+        if(this.posSerialRef >=25) {                         // waits a while without paint 
+          this.posSerialRef = 0;                             // then starts from 0 again
+          this.ixColor = 0;
+          this.ref1.setNewPosition("12-2, 10.5+1");         // start position, first increment is done before usage
+          for(GralCanvasStorage.Figure rx1 : this.rxSlave1 ) {
+            rx1.data.color = this.colorWhite;
+          }
+          this.plotArea.repaint();
+          Debugutil.stop();
         }
-        this.plotArea.repaint();
+      }
+      else {                                               // shows the output.
+        if(ixColor >=1 && ixColor < 21) {
+          this.rxSlave1[ixColor-1].data.color = this.serialOut1word.data.color;  // move content from rx Port to rx data
+        }
+        GralColor colorWord;
+        if(this.ixColor >=0 && ixColor <20) {
+          colorWord = this.colorWordsMaster[this.ixColor];
+        } else if(this.ixColor >=21) {
+          colorWord = this.colorWhite;
+        } else {
+          colorWord = this.serialOut1word.data.color;      // last step remain the color.
+        }
+        this.serialOut1word.data.color = colorWord; 
+        this.ref1.setNewPosition(GralPos.refer, GralPos.samesize, GralPos.refer + 1.0f, GralPos.samesize);
+        this.ixColor +=1;
+        this.plotArea.redraw(20, 100, true);
         Debugutil.stop();
       }
-    }
-    else {                                                 // shows the output.
-      int ixColor = (int)this.posSerialRef;                // the ix of the serial out master
-      if(ixColor >=1 && ixColor < 21) {
-        this.rxSlave1[ixColor-1].data.color = this.serialOut1word.data.color;  // move content from rx Port to rx data
-      }
-      GralColor colorWord;
-      if(ixColor >=0 && ixColor <20) {
-        colorWord = this.colorWordsMaster[ixColor];
-      } else if(ixColor >=21) {
-        colorWord = this.colorWhite;
-      } else {
-        colorWord = this.serialOut1word.data.color;        // last step remain the color.
-      }
-      this.serialOut1word.data.color = colorWord; 
-      this.ref1.pos.setPosition(this.ref1.pos, GralPos.refer, GralPos.samesize, GralPos.refer + 0.5f, GralPos.samesize);
-      
-      this.plotArea.repaint();
-      Debugutil.stop();
     }
   }
   
